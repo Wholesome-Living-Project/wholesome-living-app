137d32f02b8b51c1bc93b15bd0408e511f5ee84a
rnichi1 <remus.nichiteanu@uzh.ch>
2023-02-12 00:26:36 +0100

    update deps

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
     plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
       'react-native-reanimated/plugin',
-      // https://expo.github.io/router/docs/intro#configure-the-babel-plugin
       require.resolve('expo-router/babel'),
     ],
   }
