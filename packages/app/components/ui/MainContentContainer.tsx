495db129ffca6c774c70665f43e7a8c715624dbf
radubauzh <rafael.dubach@uzh.ch>
2023-04-20 14:03:31 +0200

    yeaboiiii

---
+++ packages/app/components/ui/MainContentContainer.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { COLORS, HEADER_HEIGHT, OUTER_BORDER_RADIUS, SIDEBAR_WIDTH, SPACING } from 'app/theme/theme'
+import { Flex } from 'axelra-styled-bootstrap-grid'
+
+
+export const MainContentContainer = styled(Flex)`
+  border: 1px solid ${COLORS.BLACK};
+  background-color: ${COLORS.TAB_BAR};
+  border-radius: ${OUTER_BORDER_RADIUS}px;
+  height: 100%;
+  margin-top: ${SPACING * 2}px;
+
+  @media only screen and (min-width: ${(p) => p.theme.breakPoints.sm}px) {
+    margin-left: ${SPACING * 2}px;
+    height: calc(100% - ${HEADER_HEIGHT}px);
+  }
+`
