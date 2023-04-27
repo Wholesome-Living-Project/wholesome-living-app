0f7fd0eaf25ff56542b69357ea487881911cd3f4
radubauzh <rafael.dubach@uzh.ch>
2023-04-27 16:01:33 +0200

    update

--- packages/app/components/discover/Plugin.tsx
+++ packages/app/components/discover/Plugin.tsx
@@ -1,7 +1,5 @@
-import PluginBanner from 'app/components/discover/PluginBanner'
 import { PluginType } from 'app/helpers/pluginList'
 import { Regular } from 'app/theme/typography'
-import { useRootNavigation } from 'expo-router'
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import styled from 'styled-components'
@@ -13,23 +11,9 @@ const Container = styled(TouchableOpacity)`
 
 const Plugin = (plugin: PluginType) => {
   const { title, color, icon, faIcon, materialIcon, ionIcon } = plugin
-  const navigation = useRootNavigation()
 
   return (
-    <Container
-      onPress={() =>
-        navigation?.navigate(
-          'plugin' as never,
-          {
-            color,
-            icon,
-            faIcon,
-            materialIcon,
-            ionIcon,
-          } as never
-        )
-      }>
-      <PluginBanner {...plugin} size={60} />
+    <Container>
       <Regular center>{title}</Regular>
     </Container>
   )
