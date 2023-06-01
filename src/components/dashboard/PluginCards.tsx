2fa73a05c57e83b448044b42dd4ec2b2b2f65ece
rnichi1 <remus.nichiteanu@uzh.ch>
2023-06-01 11:43:24 +0200

    make plugin cards component more generic

--- src/components/dashboard/PluginCards.tsx
+++ src/components/dashboard/PluginCards.tsx
@@ -1,12 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactNode } from 'react'
 import { ScrollView } from 'react-native'
+import { UserPluginName } from '../../../api/openapi'
+import { useUser } from '../../hooks/useUser'
 import { Heading4 } from '../../theme/typography'
 import Spacer from '../ui/Spacer'
 import FinanceDetailedBanner from './plugins/FinanceDetailedBanner'
 import MeditateDetailedBanner from './plugins/MeditateDetailedBanner'
 import RunDetailedBanner from './plugins/RunDetailedBanner'
 import { SectionTitleContainer } from './SharedStyles'
-import { useUser } from "../../hooks/useUser";
+
+const Cards: { [key in UserPluginName]: ReactNode } = {
+  [UserPluginName.PluginNameFinance]: <FinanceDetailedBanner />,
+  [UserPluginName.PluginNameMeditation]: <MeditateDetailedBanner />,
+  [UserPluginName.PluginNameElevator]: <RunDetailedBanner />,
+}
 
 const PluginCards = () => {
   const { user } = useUser()
@@ -17,30 +24,12 @@ const PluginCards = () => {
         <Heading4>Your Plugins</Heading4>
       </SectionTitleContainer>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {user?.plugins?.includes('meditation') ? (
-          <>
+        {user?.plugins?.map((plugin) => (
+          <Fragment key={plugin}>
             <Spacer x={2} />
-            <MeditateDetailedBanner />
-          </>
-        ) : (
-          <></>
-        )}
-        {user?.plugins?.includes('finance') ? (
-          <>
-            <Spacer x={2} />
-            <FinanceDetailedBanner />
-          </>
-        ) : (
-          <></>
-        )}
-        {user?.plugins?.includes('elevator') ? (
-          <>
-            <Spacer x={2} />
-            <RunDetailedBanner />
-          </>
-        ) : (
-          <></>
-        )}
+            {Cards[plugin]}
+          </Fragment>
+        ))}
         <Spacer x={2} />
       </ScrollView>
     </Fragment>
--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,24 +1,11 @@
 import React, { useState } from 'react'
 import { ScrollView, View } from 'react-native'
-import styled from 'styled-components'
 import DashboardHeader from '../../components/dashboard/DashboardHeader'
 import Discover from '../../components/dashboard/Discover'
 import Forest from '../../components/dashboard/Forest'
 import PluginCards from '../../components/dashboard/PluginCards'
-import { Flex } from '../../components/ui/Flex'
 import Spacer from '../../components/ui/Spacer'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions'
-import { SPACING } from '../../theme/theme'
-
-const AppTitleContainer = styled(Flex)`
-  width: 100%;
-  padding: ${SPACING * 2}px;
-`
-
-const SectionTitleContainer = styled(Flex)`
-  width: 100%;
-  padding: ${SPACING * 2}px;
-`
 
 export function DashboardScreen() {
   const { windowHeight } = useWindowDimensions()
