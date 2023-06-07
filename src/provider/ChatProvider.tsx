f7e4d876941caba8930beac1a386c866fc0f0d42
claudioge <claudio.gebbia@hotmail.com>
2023-06-08 00:23:56 +0200

    promt engineering

--- src/provider/ChatProvider.tsx
+++ src/provider/ChatProvider.tsx
@@ -6,6 +6,7 @@ type ChatType = {
   sendMessage: (message: string) => void
   messages: Message[]
   setMessages: (st: Message[]) => void
+  generateSettings: () => void
 }
 
 const ChatContext = createContext<ChatType>({} as ChatType)
@@ -21,10 +22,25 @@ const useProvideChat = (): ChatType => {
     {
       role: 'system',
       content:
-        'As a friendly habit formation consultant, your goal is to help users discover a specific easily performed, and trackable habit. Use fewer than 5 concise questions to gather essential user information. Guide them through identifying the tiny habit and conditions for nuding and notification. Consider various trackable data for conditions and determine which data the user is willing to share. Summarize and end the conversation by saying you will send a notification when conditions are met. Limit your response to 20 words or less.',
+        "'As a friendly habit formation consultant, your goal is to help users discover a specific easily performed, and trackable habit. Use fewer than 5 concise questions to gather essential user information.\n" +
+        '\n' +
+        'This application has 2 Plugins that help you form habits: Meditation: to form the habit of meditating, finance to start saving money and invest for the future.\n' +
+        '\n' +
+        'For each plugin it is possible to activate notifications. Ask the user if and how often the app should remind him to use each one of the plugins meditation and finance.\n' +
+        '\n' +
+        'If the user chooses to use the finance plugin you need to ask him the following questions:\n' +
+        '1 .How much does he want to save\n' +
+        '2.What strategy does he want to use for saving, there is 3 options:\n' +
+        '\t- round up to the nearest factor of 5 and save the difference \n' +
+        '\t- save a percentage of each spending you make\n' +
+        '\t- save a fix amount every time you spend money\n' +
+        'If the user chooses to use the meditation plugin you need to ask him the following questions:\n' +
+        '1. How many times does he want to meditate\n' +
+        '\n' +
+        ' Guide them through identifying the tiny habit and conditions for nuding and notification. Consider various trackable data for conditions and determine which data the user is willing to share. Summarize and end the conversation by saying you will send a notification when conditions are met. Limit your response to 20 words or less',
     },
     {
-      content: 'hey',
+      content: "Hi! I'm your Al Coach and I'm here to help you narrow down your goal.",
       role: 'assistant',
     },
   ])
@@ -71,10 +87,64 @@ const useProvideChat = (): ChatType => {
     [messages]
   )
 
+  const generateSettings = useCallback(async () => {
+    const promt =
+      'SettingsCreateSettingsRequest {\n' +
+      'elevator:{\n' +
+      'amountNotifications: number;\n' +
+      'goal: number;\n' +
+      'notifications: boolean;\n' +
+      'periodNotifications: {"weekly"|"monthly"|"daily"};\n' +
+      '}\n' +
+      'enabledPlugins:{}\n' +
+      'finance:{\n' +
+      'amountNotifications: number;\n' +
+      'investmentGoal: number;\n' +
+      'investmentTimeGoal: number;\n' +
+      'notifications: boolean;\n' +
+      'periodNotifications: {"weekly"|"monthly"|"daily"};\n' +
+      'strategy: SettingsStrategyType;\n' +
+      'strategyAmount: number;\n' +
+      '}\n' +
+      'meditation:{\n' +
+      'amountNotifications: number;\n' +
+      'meditationTimeGoal: number;\n' +
+      'notifications: boolean;\n' +
+      'periodNotifications: {"weekly"|"monthly"|"daily"};\n' +
+      '}' +
+      'use the chat above and the to fill out this json, use the given types to fill it out and only respond with the json nothing else'
+
+    try {
+      const res = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            ...messages,
+            {
+              content: promt,
+              role: 'user',
+            },
+          ],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + Constants.manifest?.extra?.CHATGPT_API_KEY,
+          },
+        }
+      )
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+    }
+  }, [messages])
+
   return {
     sendMessage,
     messages,
     setMessages,
+    generateSettings,
   }
 }
 
