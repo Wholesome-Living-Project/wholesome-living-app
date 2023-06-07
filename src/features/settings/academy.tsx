b1fe3f54144e6c4bb5b9152efa2d6c4cd362c5a1
rnichi1 <remus.nichiteanu@uzh.ch>
2023-06-07 23:17:55 +0200

    line chart texts

--- src/features/settings/academy.tsx
+++ src/features/settings/academy.tsx
@@ -1,14 +1,13 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
-import { ScrollView, Text, View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import styled from 'styled-components'
 import { SectionTitleContainer } from '../../components/dashboard/SharedStyles'
 import Background from '../../components/ui/Background'
-import { Flex } from '../../components/ui/Flex'
-import { COLORS, OUTER_BORDER_RADIUS } from '../../theme/theme'
-import { Heading4, Heading5 } from '../../theme/typography'
+import { Body, Heading4, Heading5 } from '../../theme/typography'
 
+import Spacer from '../../components/ui/Spacer'
 import {
   AHV,
   budgeting,
@@ -22,32 +21,31 @@ import {
   mental,
 } from './academyText'
 
-const Container = styled(Flex)`
-  position: relative;
-  background-color: ${COLORS.GREY};
-  border-radius: ${OUTER_BORDER_RADIUS}px;
-`
-
-const JustifiedText = styled(Text)`
+const JustifiedText = styled(Body)`
   text-align: justify;
 `
 
 const PensionScreen = () => (
   <Background>
     <ScrollView>
-      <Container>
-        <SectionTitleContainer>
-          <Heading4>Pension</Heading4>
-          <View>
-            <Heading5>1st Pillar: AHV</Heading5>
-            <JustifiedText>{AHV}</JustifiedText>
-            <Heading5>2nd Pillar: BVG</Heading5>
-            <JustifiedText>{BVG}</JustifiedText>
-            <Heading5>3rd Pillar: Individual</Heading5>
-            <JustifiedText>{Individual}</JustifiedText>
-          </View>
-        </SectionTitleContainer>
-      </Container>
+      <SectionTitleContainer>
+        <Heading4>Pension</Heading4>
+        <Spacer x={2} />
+        <View>
+          <Heading5>1st Pillar: AHV</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{AHV}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>2nd Pillar: BVG</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{BVG}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>3rd Pillar: Individual</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{Individual}</JustifiedText>
+          <Spacer x={4} />
+        </View>
+      </SectionTitleContainer>
     </ScrollView>
   </Background>
 )
@@ -55,21 +53,28 @@ const PensionScreen = () => (
 const FinanceScreen = () => (
   <Background>
     <ScrollView>
-      <Container>
-        <SectionTitleContainer>
-          <Heading4>Finance</Heading4>
-          <View>
-            <Heading5>Historical Market Analysis</Heading5>
-            <JustifiedText>{historicalmarketanalysis}</JustifiedText>
-            <Heading5>Compound Interest</Heading5>
-            <JustifiedText>{compoundinterest}</JustifiedText>
-            <Heading5>How can I invest?</Heading5>
-            <JustifiedText>{howtoinvest}</JustifiedText>
-            <Heading5>Budgeting</Heading5>
-            <JustifiedText>{budgeting}</JustifiedText>
-          </View>
-        </SectionTitleContainer>
-      </Container>
+      <SectionTitleContainer>
+        <Heading4>Finance</Heading4>
+        <Spacer x={2} />
+        <View>
+          <Heading5>Historical Market Analysis</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{historicalmarketanalysis}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>Compound Interest</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{compoundinterest}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>How can I invest?</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{howtoinvest}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>Budgeting</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{budgeting}</JustifiedText>
+          <Spacer x={4} />
+        </View>
+      </SectionTitleContainer>
     </ScrollView>
   </Background>
 )
@@ -77,19 +82,23 @@ const FinanceScreen = () => (
 const AntiAgingScreen = () => (
   <Background>
     <ScrollView>
-      <Container>
-        <SectionTitleContainer>
-          <Heading4>Anti-Aging</Heading4>
-          <View>
-            <Heading5>Glucose</Heading5>
-            <JustifiedText>{glucose}</JustifiedText>
-            <Heading5>Exercise and Movement</Heading5>
-            <JustifiedText>{exercise}</JustifiedText>
-            <Heading5>Mental Wellbeing</Heading5>
-            <JustifiedText>{mental}</JustifiedText>
-          </View>
-        </SectionTitleContainer>
-      </Container>
+      <SectionTitleContainer>
+        <Heading4>Anti-Aging</Heading4>
+        <Spacer x={2} />
+        <View>
+          <Heading5>Glucose</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{glucose}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>Exercise and Movement</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{exercise}</JustifiedText>
+          <Spacer x={4} />
+          <Heading5>Mental Wellbeing</Heading5>
+          <Spacer x={1} />
+          <JustifiedText>{mental}</JustifiedText>
+        </View>
+      </SectionTitleContainer>
     </ScrollView>
   </Background>
 )
@@ -102,6 +111,7 @@ const AcademyScreen = () => (
       name="Pension"
       component={PensionScreen}
       options={{
+        headerShown: false,
         tabBarIcon: ({ color, size }) => (
           <Ionicons name="wallet-outline" color={color} size={size} />
         ),
@@ -111,6 +121,7 @@ const AcademyScreen = () => (
       name="Finance"
       component={FinanceScreen}
       options={{
+        headerShown: false,
         tabBarIcon: ({ color, size }) => (
           <Ionicons name="analytics-outline" color={color} size={size} />
         ),
@@ -120,6 +131,7 @@ const AcademyScreen = () => (
       name="Anti-Aging"
       component={AntiAgingScreen}
       options={{
+        headerShown: false,
         tabBarIcon: ({ color, size }) => (
           <Ionicons name="heart-outline" color={color} size={size} />
         ),
